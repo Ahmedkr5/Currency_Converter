14834d13eee8606b5a9e87fb2c3b745c5b08baef
Ahmedkr5 <ahmed.khiari2@esprit.tn>
2021-10-09 17:23:09 +0100

    clearing project/creating component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import CurrencyRow from './CurrencyRow';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<>
+<h1>Convert</h1>
+<CurrencyRow></CurrencyRow>
+<div>=</div>
+<CurrencyRow></CurrencyRow>
+
+</>
   );
 }
 
